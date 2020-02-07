b616d527c65105fd8e3fe60b73d2d513b951adcd
VietIS-Intern Bình <binh.hathanh@vietis.com.vn>
2020-02-07 18:15:48 +0700

    kiki

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
+<html lang="jp">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport"
@@ -7,141 +7,191 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>株式会社青山ケアサポート</title>
     <?php require_once('template/css-external.php'); ?>
-    <link rel="stylesheet" href="template/css/index/index.css" />
+    <link rel="stylesheet" href="template/css/temp.css" />
 </head>
+<!--<body>-->
+<!--    <div class="page">-->
+<!--	    --><?php //require_once('template/header.php'); ?>
+<!--        <div class="banner">-->
+<!--            <div class="banner-bg" id="banner">-->
+<!--                <div class="slogan wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay=".5s">-->
+<!--                    <div class="slogan-big">live true</div>-->
+<!--                    <div class="slogan-big">to yourself</div>-->
+<!--                    <div class="slogan-mini">あなたらしい 暮らしを。</div>-->
+<!--                    <div class="slogan-descript">-->
+<!--                        <div>こちらに記載している文章はダミー</div>-->
+<!--                        <div>文章でございます文字の大きさや量、</div>-->
+<!--                        <div>行間等を調整し、レイアウトを確認する</div>-->
+<!--                        <div>ために配置しています。</div>-->
+<!--                        <div>後ほど本来の文章を配置しますので、</div>-->
+<!--                        <div>今暫くお待ち頂けるようお願い申し上げます。</div>-->
+<!--                        <div>こちらに記載している文章はダミー文章でございます。</div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                -->
+<!--            </div>-->
+<!--        </div>-->
+<!--        -->
+<!--        <section class="introduce">-->
+<!--            <div class="introduce-big-ttl wow fadeInUp">business</div>-->
+<!--            <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">事業紹介</div>-->
+<!--            <div class="introduce-content">-->
+<!--                <div class="introduce-content-element element-first">-->
+<!--                    <div class="introduce-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">介護事業</div>-->
+<!--                    <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s"">care and welfare</div>-->
+<!--                </div>-->
+<!--                <div class="introduce-content-element element-second">-->
+<!--                    <div class="introduce-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">海外事業</div>-->
+<!--                    <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">overseas business</div>-->
+<!--                    <div class="element-second-descript">青山ケアサポートは、 <br>日本政府が選ぶ優良法人に選定されています。</div>-->
+<!--                </div>-->
+<!--                <div class="introduce-content-element element-third">-->
+<!--                    <div class="img-top">-->
+<!--                        <div class="img-top-mini-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">高齢者旅行・外出支援事業</div>-->
+<!--                        <div class="introduce-big-ttl mt-0 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">青山シニアトラベル</div>-->
+<!--                        <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">aoyama senior travel</div>-->
+<!--                    </div>-->
+<!--                    <div class="img-bottom">-->
+<!--                        <div class="introduce-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">ビルクリーニング<br>事業</div>-->
+<!--                        <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">building cleaning</div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--        -->
+<!--        <section>-->
+<!--            <div class="row main-content news">-->
+<!--                <div class="col-md-2 wow fadeInUp">-->
+<!--                    <div class="new-big-ttl">news</div>-->
+<!--                    <div class="new-ttl">お知らせ</div>-->
+<!--                </div>-->
+<!--                <div class="col-md-10 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">-->
+<!--                    <div class="new-element">-->
+<!--                        <div class="new-datetime">2020.03.01</div>-->
+<!--                        <div class="new-mark">プレスリリース</div>-->
+<!--                        <div class="new-content">-->
+<!--                            <a href="#">経営再建中の液晶パネル大手ジャパンディスプレイは２１日、元社員が約４年間にわたって不正経理を繰り 返し、総額で５億７８００万円を着服していたと発表したダミー</a>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="new-element">-->
+<!--                        <div class="new-datetime">2020.03.01</div>-->
+<!--                        <div class="new-mark">プレスリリース</div>-->
+<!--                        <div class="new-content">-->
+<!--                            <a href="#">山梨県道志村のキャンプ場で千葉県成田市の小学１年、小倉美咲さん（７）が行方不明ダミー</a>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <div class="new-element">-->
+<!--                        <div class="new-datetime">2020.03.01</div>-->
+<!--                        <div class="new-mark">プレスリリース</div>-->
+<!--                        <div class="new-content">-->
+<!--                            <a href="#">日中両政府が、日本産牛肉の対中輸出を再開する方向で最終調整していることが21日、分かったダミー</a>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="news-btn">-->
+<!--                <button class="btn button-boxy wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">read more</button>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--        -->
+<!--        <section>-->
+<!--            <div class="main-content new-banner">-->
+<!--                <div class="new-banner-row">-->
+<!--                    <div class="new-banner-col wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">-->
+<!--                        <span>news banner</span>-->
+<!--                    </div>-->
+<!--                    <div class="new-banner-col wow fadeInUp" data-wow-duration="1s" data-wow-delay=".8s">-->
+<!--                        <span>news banner</span>-->
+<!--                    </div>-->
+<!--                    <div class="new-banner-col wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.2s">-->
+<!--                        <span>news banner</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--        -->
+<!--        <section>-->
+<!--            <div class="contact">-->
+<!--                <div class="contact-row">-->
+<!--                    <div class="contact-company">-->
+<!--                        <div class="contact-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">company</div>-->
+<!--                        <div class="contact-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">会社案内</div>-->
+<!--                    </div>-->
+<!--                    <div class="contact-recruit">-->
+<!--                        <div class="contact-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">recruit</div>-->
+<!--                        <div class="contact-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">採用情報</div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--        -->
+<!--        <section>-->
+<!--            <div class="contact-row">-->
+<!--                <div class="contact-info">-->
+<!--                    <div class="contact-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">contact</div>-->
+<!--                    <div class="contact-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">お問い合わせ</div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </section>-->
+<!--	-->
+<!--	    --><?php ///* require_once('template/footer.php'); */?>
+<!--    </div>-->
+<!--    -->
+<!--    --><?php //require_once('template/js-external.php'); ?>
+<!--    <script>-->
+<!--        $(function(){-->
+<!--            $("#banner").css("backgroundPosition", "-40px");-->
+<!--        });-->
+<!--    </script>-->
+<!--</body>-->
 <body>
-    <div class="page">
-	    <?php require_once('template/header.php'); ?>
-        <div class="banner">
-            <div class="banner-bg" id="banner">
-                <div class="slogan wow fadeInLeft" data-wow-duration="1.5s" data-wow-delay=".5s">
-                    <div class="slogan-big">live true</div>
-                    <div class="slogan-big">to yourself</div>
-                    <div class="slogan-mini">あなたらしい 暮らしを。</div>
-                    <div class="slogan-descript">
-                        <div>こちらに記載している文章はダミー</div>
-                        <div>文章でございます文字の大きさや量、</div>
-                        <div>行間等を調整し、レイアウトを確認する</div>
-                        <div>ために配置しています。</div>
-                        <div>後ほど本来の文章を配置しますので、</div>
-                        <div>今暫くお待ち頂けるようお願い申し上げます。</div>
-                        <div>こちらに記載している文章はダミー文章でございます。</div>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-        
-        <section class="introduce">
-            <div class="introduce-big-ttl wow fadeInUp">business</div>
-            <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">事業紹介</div>
-            <div class="introduce-content">
-                <div class="introduce-content-element element-first">
-                    <div class="introduce-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">介護事業</div>
-                    <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s"">care and welfare</div>
-                </div>
-                <div class="introduce-content-element element-second">
-                    <div class="introduce-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">海外事業</div>
-                    <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">overseas business</div>
-                    <div class="element-second-descript">青山ケアサポートは、 <br>日本政府が選ぶ優良法人に選定されています。</div>
-                </div>
-                <div class="introduce-content-element element-third">
-                    <div class="img-top">
-                        <div class="img-top-mini-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">高齢者旅行・外出支援事業</div>
-                        <div class="introduce-big-ttl mt-0 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">青山シニアトラベル</div>
-                        <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">aoyama senior travel</div>
-                    </div>
-                    <div class="img-bottom">
-                        <div class="introduce-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">ビルクリーニング<br>事業</div>
-                        <div class="introduce-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">building cleaning</div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        
-        <section>
-            <div class="row main-content news">
-                <div class="col-md-2 wow fadeInUp">
-                    <div class="new-big-ttl">news</div>
-                    <div class="new-ttl">お知らせ</div>
-                </div>
-                <div class="col-md-10 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
-                    <div class="new-element">
-                        <div class="new-datetime">2020.03.01</div>
-                        <div class="new-mark">プレスリリース</div>
-                        <div class="new-content">
-                            <a href="#">経営再建中の液晶パネル大手ジャパンディスプレイは２１日、元社員が約４年間にわたって不正経理を繰り 返し、総額で５億７８００万円を着服していたと発表したダミー</a>
-                        </div>
-                    </div>
-                    <div class="new-element">
-                        <div class="new-datetime">2020.03.01</div>
-                        <div class="new-mark">プレスリリース</div>
-                        <div class="new-content">
-                            <a href="#">山梨県道志村のキャンプ場で千葉県成田市の小学１年、小倉美咲さん（７）が行方不明ダミー</a>
-                        </div>
-                    </div>
-                    <div class="new-element">
-                        <div class="new-datetime">2020.03.01</div>
-                        <div class="new-mark">プレスリリース</div>
-                        <div class="new-content">
-                            <a href="#">日中両政府が、日本産牛肉の対中輸出を再開する方向で最終調整していることが21日、分かったダミー</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="news-btn">
-                <button class="btn button-boxy wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">read more</button>
-            </div>
-        </section>
-        
-        <section>
-            <div class="main-content new-banner">
-                <div class="new-banner-row">
-                    <div class="new-banner-col wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
-                        <span>news banner</span>
-                    </div>
-                    <div class="new-banner-col wow fadeInUp" data-wow-duration="1s" data-wow-delay=".8s">
-                        <span>news banner</span>
-                    </div>
-                    <div class="new-banner-col wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.2s">
-                        <span>news banner</span>
-                    </div>
-                </div>
-            </div>
-        </section>
-        
-        <section>
-            <div class="contact">
-                <div class="contact-row">
-                    <div class="contact-company">
-                        <div class="contact-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">company</div>
-                        <div class="contact-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">会社案内</div>
-                    </div>
-                    <div class="contact-recruit">
-                        <div class="contact-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">recruit</div>
-                        <div class="contact-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">採用情報</div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        
-        <section>
-            <div class="contact-row">
-                <div class="contact-info">
-                    <div class="contact-big-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">contact</div>
-                    <div class="contact-ttl wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">お問い合わせ</div>
-                </div>
-            </div>
-        </section>
+	<header>
+		<nav class="navbar navbar-expand-xl navbar-dark bg-secondary">
+			<a class="navbar-brand logo" href="#">
+				<img src="template/images/logo_hdr.svg"  alt="青山ケアサポート" />
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContentXL" aria-controls="navbarSupportedContentXL" aria-expanded="false" aria-label="Toggle navigation">
+<!--				<span class="navbar-toggler-icon"></span>-->
+				<span class="icon-bar top-bar"></span>
+				<span class="icon-bar middle-bar"></span>
+				<span class="icon-bar bottom-bar"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContentXL">
+				<ul class="navbar-nav ml-auto menu">
+					<li class="nav-item">
+						<a class="nav-link" href="#">事業案内</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">会社案内</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">協力病院・施設</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">お知らせ</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">採用情報</a>
+					</li>
+					<li class="nav-item nav-item-contact">
+						<a class="nav-link" href="#">
+							<img src="template/images/contact.svg"  style="width: 64px"/>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
 	
-	    <?php require_once('template/footer.php'); ?>
-    </div>
-    
-    <?php require_once('template/js-external.php'); ?>
-    <script>
-        $(function(){
-            $("#banner").css("backgroundPosition", "-40px");
-        });
-    </script>
+	<section>
+	
+	</section>
+
+	<footer>
+	
+	</footer>
+	<?php require_once('template/js-external.php'); ?>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
--- template/css-external.php
+++ template/css-external.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="template/css/lib/bootstrap.min.css" />
 <link rel="stylesheet" href="template/css/lib/animate.min.css" />
-<link rel="stylesheet" href="template/css/style.css" />
+<!--<link rel="stylesheet" href="template/css/style.css" />-->
